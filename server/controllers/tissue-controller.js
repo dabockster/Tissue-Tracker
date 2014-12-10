2a3a1363a1894e04e468d746400e49fc13c498f4
Steven Bock <stevenbockster@gmail.com>
2014-12-10 01:30:12 -0800

    Refactoring

--- server/controllers/tissue-controller.js
+++ server/controllers/tissue-controller.js
@@ -2,14 +2,20 @@
 var Tissue = require('../models/tissue');
 
 module.exports.create = function(req, res){
+	//create new tissue based on request data from client
 	var tissue = new Tissue(req.body);
+
+	//save to database
 	tissue.save(function (err, result) {
+		//send as JSON
     	res.json(result);
 	});
 };
 
 module.exports.list = function (req, res){
+	//grab tissues from database
 	Tissue.find({}, function (err, tissues) {
+		//recieve as JSON
     	res.json(tissues);
   });
 };
